1bfe89283078912e7add2ea46b4cba8287680e8d
Maxwell929 <maximilian9219@gmail.com>
2021-11-29 22:31:58 +0100

    changes

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 double nettoRechner(double gewinn, vector<double> steuersaetze, vector<double> bereichsgrenzen) {
 
-    static double summeSteuer{0.00};
-    double steuer{0.00};
-    double freibetrag{11000.00};
-    int sizeSteuersaetze = size(steuersaetze) - 1;
-    int sizeBereichsgrenzen = size(bereichsgrenzen) - 1;
+    static double summeSteuer{0};
+    double steuer{0};
+    double freibetrag{11000};
+    int sizeSteuersaetze = steuersaetze.size() - 1;
+    int sizeBereichsgrenzen = bereichsgrenzen.size() - 1;
 
 
     if (gewinn <= freibetrag) {
@@ -28,13 +28,24 @@ double nettoRechner(double gewinn, vector<double> steuersaetze, vector<double> b
     return nettoRechner(gewinn, steuersaetze, bereichsgrenzen);
 }
 
-
 int main() {
     double bruttoGewinn{0};
     const vector<double> vectSteuersaetzte{0.00, 0.25, 0.35, 0.42, 0.48, 0.50, 0.55};
-    const vector<double> vectBereichsgrenze{0.00, 11000.00, 18000.00, 31000.00, 60000.00, 90000.00, 1000000.00};
+    const vector<double> vectBereichsgrenze{0, 11000, 18000, 31000, 60000, 90000, 1000000};
 
+    cout << "Bitte geben Sie den zu versteuernden Gewinn ein:" << endl;
+    cin >> bruttoGewinn;
 
-    cout << nettoRechner(bruttoGewinn, vectSteuersaetzte, vectBereichsgrenze) << endl;
+    while (cin.fail() || bruttoGewinn <= 0) {
+        cout << "Leider ist das keine gültige Zahl. Probieren Sie es nochmal:\n";
 
+        cin.clear();
+
+        cin.ignore(500, '\n');
+
+        cin >> bruttoGewinn;
+    }
+
+    cout << "Der zu bezahlende Steuerbetrag auf Ihren Gewinn beträgt: "
+         << nettoRechner(bruttoGewinn, vectSteuersaetzte, vectBereichsgrenze) << "€" << endl;
 }
