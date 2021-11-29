c4ae7917d51d7b0c1c128d0c500e801354389a1d
Maximilian <maximilian9219@gmail.com>
2021-11-29 18:03:10 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double nettoRechner(double gewinn, vector<double> steuersaetze, vector<double> bereichsgrenzen) {
+
+    static double summeSteuer{0};
+    double steuer{0};
+    double freibetrag = 11000.00;
+    int sizeSteuersaetze = size(steuersaetze) - 1;
+    int sizeBereichsgrenzen = size(bereichsgrenzen) - 1;
+
+
+    if (gewinn <= freibetrag) {
+        return summeSteuer;
+    } else if (gewinn > freibetrag) {
+        if (gewinn > bereichsgrenzen[sizeBereichsgrenzen]) {
+            steuer = (gewinn - bereichsgrenzen[sizeBereichsgrenzen]) * steuersaetze[sizeSteuersaetze];
+            gewinn = gewinn - (gewinn - bereichsgrenzen[sizeBereichsgrenzen]);
+            summeSteuer += steuer;
+        }
+    }
+
+
+    steuersaetze.pop_back();
+    bereichsgrenzen.pop_back();
+
+    return nettoRechner(gewinn, steuersaetze, bereichsgrenzen);
+
+
+}
+
+
+int main() {
+    
+    vector<double> vectSteuersaetzte{0.00, 0.25, 0.35, 0.42, 0.48, 0.50, 0.55};
+    vector<double> vectBereichsgrenze{0.00, 11000.00, 18000, 31000, 60000, 90000, 1000000};
+    
+    
+
+    cout << nettoRechner(40000, vectSteuersaetzte, vectBereichsgrenze) << endl;
+
+}
