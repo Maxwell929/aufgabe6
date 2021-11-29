5fc045ee4c3125dfe8178a83ea18ac12f91b3b2b
Maximilian <maximilian9219@gmail.com>
2021-11-29 18:18:37 +0100

    added bruttoGewinn

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 double nettoRechner(double gewinn, vector<double> steuersaetze, vector<double> bereichsgrenzen) {
 
-    static double summeSteuer{0};
-    double steuer{0};
+    static double summeSteuer{0.00};
+    double steuer{0.00};
     double freibetrag{11000.00};
     int sizeSteuersaetze = size(steuersaetze) - 1;
     int sizeBereichsgrenzen = size(bereichsgrenzen) - 1;
@@ -33,11 +33,11 @@ double nettoRechner(double gewinn, vector<double> steuersaetze, vector<double> b
 
 
 int main() {
-
+    double bruttoGewinn{0};
     const vector<double> vectSteuersaetzte{0.00, 0.25, 0.35, 0.42, 0.48, 0.50, 0.55};
-    const vector<double> vectBereichsgrenze{0.00, 11000.00, 18000, 31000, 60000, 90000, 1000000};
+    const vector<double> vectBereichsgrenze{0.00, 11000.00, 18000.00, 31000.00, 60000.00, 90000.00, 1000000.00};
 
 
-    cout << nettoRechner(1500000, vectSteuersaetzte, vectBereichsgrenze) << endl;
+    cout << nettoRechner(bruttoGewinn, vectSteuersaetzte, vectBereichsgrenze) << endl;
 
 }
